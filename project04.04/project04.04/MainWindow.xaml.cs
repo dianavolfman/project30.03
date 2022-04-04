68c7c0e84aa11d3f0c2d9bb835f0e351f897140c
dianavolfman <dianavolfman0@gmail.com>
2022-04-04 09:21:14 +0300

    04.04

---
+++ project04.04/project04.04/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace project04._04
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAvtorization_Click(object sender, RoutedEventArgs e)
+        {
+           // Avtorization project = projectEntities.getContext().Avtorizations();
+            //if (textLogin.Text.Equals(project.Avtorizations.)) { }
+            ClientWindow taskWindow = new ClientWindow();
+            taskWindow.Show();
+            this.Close();
+        }
+    }
+}
